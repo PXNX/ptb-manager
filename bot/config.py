93e2c544f9f93cbe2025a08ddc5fc89de5cae7d0
nyx <loool1912@web.de>
2025-11-23 20:54:18 +0100

    using container specific paths

--- bot/config.py
+++ bot/config.py
@@ -6,8 +6,15 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 ALLOWED_USER_IDS = [int(uid) for uid in os.environ.get('ALLOWED_USER_IDS', '').split(',') if uid]
-PROJECTS_BASE = os.path.expanduser('~/projects')
-QUADLETS_DIR = os.path.expanduser("~/.config/containers/systemd")
+
 PODMAN_URL = os.getenv('PODMAN_URL', '')  # e.g., tcp://host.containers.internal:8888
 DEFAULT_GITHUB_ORG = os.getenv('DEFAULT_GITHUB_ORG', 'PXNX')
 PODMAN_CMD = os.getenv('PODMAN_CMD', '/usr/bin/podman')
+
+# Use containerized paths if running in container, otherwise use host paths
+if os.getenv('CONTAINER') == 'true':
+    PROJECTS_BASE = os.getenv('PROJECTS_BASE', '/host/projects')
+    QUADLETS_DIR = os.getenv('QUADLETS_DIR', '/host/quadlets')
+else:
+    PROJECTS_BASE = os.path.expanduser(os.getenv('PROJECTS_BASE', '~/projects'))
+    QUADLETS_DIR = os.path.expanduser('~/.config/containers/systemd')
